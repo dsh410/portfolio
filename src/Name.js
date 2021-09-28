5563c66ba20030a211decef8fb32a850f9cbc5a1
dsh410 <dsh410@gmail.com>
2021-09-28 17:26:01 -0500

    added cellphone info and git hub link to my profile.

--- src/Name.js
+++ src/Name.js
@@ -1,8 +1,17 @@
-
+import { FaPhone,FaGithub } from 'react-icons/fa';
 const Name = () => {
     return (
         <div className="name-container">
             <h2>Demetri Selmer</h2>
+            <div className="cellPhoneIcon">
+                <FaPhone />
+                <p>312.605.2652</p>
+                <a className="gitHubLink" href="https://github.com/dsh410">
+                    <FaGithub />
+                  
+                </a>
+               
+            </div>
         </div>
     )
 }
