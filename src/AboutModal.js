99c7b44f392b608e60bc29e39189a7e5a544211f
dsh410 <dsh410@gmail.com>
2021-12-07 07:47:12 -0600

    made more grammer changes to about me.

--- src/AboutModal.js
+++ src/AboutModal.js
@@ -14,7 +14,7 @@ function AboutModal(props) {
       <Modal.Body>
         <h4>Hi My Name Is Demetri</h4>
         <p>
- Iam a Front end web developer skilled in HTML, CSS, JavaScript, Bootstrap, Git, React l, and other UI libraries with as love for front end frameworks like React. A professional with a desire to learn new skills and improve with feedback. I believe I would be a great asset to your team. please click on my projects tab if you will like to see my skills in practice. 
+ Iam a Front end web developer skilled in HTML, CSS, JavaScript, Bootstrap, Git, React l, and other UI libraries with a love for front end frameworks like React. A professional with a desire to learn new skills and improve with feedback. I believe I would be a great asset to your team. please click on my projects tab if you will like to see my skills in practice. 
         </p>
       </Modal.Body>
       <Modal.Footer>
